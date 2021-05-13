968fd24d53bfabc50198546c9f300a6fc16c9b3b
QuaKe8782 <mastermind4560@gmail.com>
2021-05-13 22:47:52 +0100

    pages/parts/[id]/index: Code formatting changes

--- pages/parts/[id]/index.js
+++ pages/parts/[id]/index.js
@@ -7,7 +7,9 @@ const about = ({ part }) => {
 }
 
 export const getServerSideProps = async (context) => {
-  const response = await fetch(`/api/parts/${context.params.id}`)
+  const response = await fetch(
+    `http://127.0.0.1:3000/api/parts/${context.params.id}`
+  )
   const part = await response.json()
   return {
     props: {
