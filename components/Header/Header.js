7e4e142ef4fc9752ad0dd1f854934a0a7c7ff7ee
QuaKe8782 <mastermind4560@gmail.com>
2021-05-15 22:45:10 +0100

    Rearrange meta tags and add twitter: tags

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -10,31 +10,32 @@ const Header = ({
   return (
     <Head>
       <title>{title ? `PartMatcher ${title}` : "PartMatcher"}</title>
-      <meta
-        property="og:title"
-        content={title ? `PartMatcher ${title}` : "PartMatcher"}
-      />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content={image ? image : "/images/logo128.png"}
-      />
-      <meta />
+
+      {/* Standard HTML Tags */}
+      <meta name="theme-color" content="#14d18c" />
+      <meta name="keywords" content={ "pc, parts, computer, components, part, component" + (keywords ? ", " + keywords.join(", ") : "") }/>
+      <meta name="rating" content="safe for kids" />
+      <meta name="target" content="all" />
+      <meta name="audience" content="all" />
+      <meta name="coverage" content="Worldwide" />
+      <meta name="distribution" content="Global" />
+      <meta name="description" content={description} />
+
+      {/* Open Graph */}
       {url && <meta property="og:url" content={url} />}
-      <meta property="theme-color" content="#14d18c" />
-      <meta
-        name="keywords"
-        content={
-          "pc, parts, computer, components, part, component" +
-          (keywords ? ", " + keywords.join(", ") : "")
-        }
-      />
-      <meta property="rating" content="safe for kids" />
-      <meta property="target" content="all" />
-      <meta property="audience" content="all" />
-      <meta property="coverage" content="Worldwide" />
-      <meta property="distribution" content="Global" />
-      <meta property="description" content="PC parts. Simplified." />
+      <meta property="og:title" content={title ? `PartMatcher ${title}` : "PartMatcher"} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image ? image : "/images/logo128.png"} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content={window.location.host} />
+      <meta property="twitter:url" content={window.location.href} />
+      <meta name="twitter:title" content={title ? `PartMatcher ${title}` : "PartMatcher"} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image ? image : "/images/logo128.png"} />
+
     </Head>
   )
 }
