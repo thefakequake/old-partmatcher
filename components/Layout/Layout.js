6635bd3b5b9e2a32d5ee42ed14c4576e7ff3ed6d
QuaKe8782 <mastermind4560@gmail.com>
2021-05-16 21:16:56 +0100

    Fix bug with favicon not appearing

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -6,15 +6,18 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
       </Head>
       <Navbar />
-      {children}
+      <div id="pageContent">
+        {children}
+      </div>
       <Footer />
     </>
   )
