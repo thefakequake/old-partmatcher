4b3c1710e36502bddd701e341614ffd871f52cc3
QuaKe8782 <mastermind4560@gmail.com>
2021-05-17 19:20:38 +0100

    Make error pages dynamic to screen width

--- pages/505.js
+++ pages/505.js
@@ -1,24 +1,28 @@
-import styles from "../styles/error.module.css"
+import styles from "../styles/505.module.css"
 import Link from "next/link"
+import Header from "../components/Header/Header"
 
 const Custom404 = () => {
   return (
-    <div className={styles.errorBox}>
-      <h1 className={styles.error}>505: Internal server error</h1>
-      <h3 className={styles.errorText}>
-        Something went wrong.{" "}
-        <Link href="https://github.com/QuaKe8782/partmatcher/issues">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.homeLink}
-          >
-            Report this on GitHub
-          </a>
-        </Link>
-        .
-      </h3>
-    </div>
+    <>
+      <Header title="Error 505" description="505: Internal server error" />
+      <div className={styles.errorBox}>
+        <h1 className={styles.error}>505: Internal server error</h1>
+        <h3 className={styles.errorText}>
+          Something went wrong.{" "}
+          <Link href="https://github.com/QuaKe8782/partmatcher/issues">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.homeLink}
+            >
+              Report on GitHub
+            </a>
+          </Link>
+          .
+        </h3>
+      </div>
+    </>
   )
 }
 
