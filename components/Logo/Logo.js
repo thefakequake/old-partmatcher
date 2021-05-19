fbff31dc5925812e92809af6c46733d417ff131b
QuaKe8782 <mastermind4560@gmail.com>
2021-05-19 17:12:10 +0100

    Change icon to SVG and change scaling of icon and text in the logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <a className={styles.logo}>
-        <img src="/images/logo64.png" width="64px" height="64px" alt="PartMatcher logo" />
+        <img src="/images/logo.svg" width="58px" height="58px" alt="PartMatcher logo" />
         <h1>PartMatcher</h1>
       </a>
     </Link>
