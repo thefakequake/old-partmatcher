10eb7480c10a3ec964abbc71efd81767cb806101
QuaKe8782 <mastermind4560@gmail.com>
2021-05-19 17:13:33 +0100

    Make seperate file for parts

--- pages/parts/[id]/index.js
+++ pages/parts/[id]/index.js
@@ -4,13 +4,18 @@ import styles from "./part.module.css"
 import Link from "next/link"
 import ImageBox from "../../../components/ImageBox/ImageBox"
 import Header from "../../../components/Header/Header"
+import parts from "../parts"
+import PartSearchPage from "../../../components/PartSearchPage/PartSearchPage"
 
-const part = ({ part }) => {
+const partUrls = parts.map((part) => part.toLowerCase().replace(/ /g, "-"))
+
+const part = ({ part, page }) => {
+  if (page !== undefined) return <PartSearchPage part={page} />
   if (!part) return <Custom404 />
 
-  const copyLinkToClipboard  = () => {
+  const copyLinkToClipboard = () => {
     navigator.clipboard.writeText(window.location.href)
-    alert("Copied Part URL to clipboard")    
+    alert("Copied Part URL to clipboard")
   }
 
   return (
@@ -27,7 +32,9 @@ const part = ({ part }) => {
             </a>
           </Link>
           <p className={styles.seperator}>•</p>
-          <p onClick={copyLinkToClipboard} className={styles.partId}>{part.part_id}</p>
+          <p onClick={copyLinkToClipboard} className={styles.partId}>
+            {part.part_id}
+          </p>
         </div>
         <h1>
           {part.manufacturer} {part.name}
@@ -68,6 +75,16 @@ const part = ({ part }) => {
 }
 
 export const getServerSideProps = async (context) => {
+  if (partUrls.includes(context.params.id))
+    return {
+      props: {
+        page: {
+          partLink: context.params.id,
+          partName: parts[partUrls.indexOf(context.params.id)]
+        }
+      }
+    }
+
   const { db } = await connectToDatabase(0)
 
   const part = await db
---
+++ pages/parts/parts.js
@@ -0,0 +1,12 @@
+const parts = [
+  "CPU",
+  "Graphics Card",
+  "Motherboard",
+  "Memory",
+  "Solid State Drive",
+  "Hard Drive",
+  "Case",
+  "Power Supply"
+]
+
+export default parts
