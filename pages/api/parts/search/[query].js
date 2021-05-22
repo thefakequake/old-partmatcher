6c76ec9d97f6bb1b771b900d0e8c5aa4033a3afd
QuaKe8782 <mastermind4560@gmail.com>
2021-05-22 22:47:13 +0100

    Add part search by type to API

--- pages/api/parts/search/[query].js
+++ pages/api/parts/search/[query].js
@@ -1,14 +1,15 @@
 import { connectToDatabase } from "../../../../utils/mongodb"
 import clamp from "../../../../utils/clamp"
 
-export default async ({ query: { query = null, limit = 10 } }, res) => {
-  if (query == null) {
+export default async ({ query: { query = null, limit = 10, type = null } }, res) => {
+  if (query === null) {
     res.status(400).json({ error: "Please provide a query parameter" })
     return
   }
+
   if (limit !== 10) {
     limit = parseInt(limit)
-    if (limit == NaN) {
+    if (limit === NaN) {
       res.status(400).json({ error: "Limit parameter couldn't be parsed!" })
       return
     }
@@ -16,11 +17,15 @@ export default async ({ query: { query = null, limit = 10 } }, res) => {
   }
 
   const { db } = await connectToDatabase()
-  const parts = await db
+  let parts = await db
     .collection("Parts")
-    .find({ $text: { $search: query } }, { projection: { _id: 0 } })
+    .find({ $text: { $search: query } }, { projection: { _id: 0 }})
     .limit(limit)
     .toArray()
+ 
+  if (type !== null) {
+    parts = parts.filter(part => part.type.toLowerCase() === type.toLowerCase())
+  }
 
   res.status(200).json(parts)
 }
