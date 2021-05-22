133a29994d69b0a115cd0fa8ece70dd20b10c72e
QuaKe8782 <mastermind4560@gmail.com>
2021-05-22 22:44:11 +0100

    Move HTML template stuff to _documnet.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,19 +1,9 @@
-import Head from "next/head"
 import Footer from "../Footer/Footer"
 import Navbar from "../Navbar/Navbar"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
-      </Head>
       <Navbar />
       <div id="pageContent">
         {children}
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
