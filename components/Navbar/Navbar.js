7747dd0bb89de29b8a45ca8c7cc55315a681883b
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:56:14 +0100

    Add part icons (some are temporary)

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,36 +4,42 @@ import Logo from "../Logo/Logo"
 import { FaTimes, FaBars } from "react-icons/fa"
 import { useState } from "react"
 import Link from "next/link"
+import { signIn, signOut, useSession } from "next-auth/client"
+import Image from "next/image"
+import NavbarDropdown from "../NavbarDropdown/NavbarDropdown"
 
-const Navbar = ({ loggedIn = false }) => {
+const Navbar = () => {
   const [mobileViewShown, setMobileViewShown] = useState(false)
+  const [session, loading] = useSession()
 
   return (
     <div className={styles.navbar}>
-      <Logo />
+      <div
+        className={styles.toggle}
+        onClick={() => setMobileViewShown(!mobileViewShown)}
+      >
+        {mobileViewShown ? <FaTimes /> : <FaBars />}
+      </div>
+      <Logo className={styles.logo} />
       <nav
         className={
           styles.navLinks + (mobileViewShown ? " " + styles.active : "")
         }
       >
-        {pages
-          .filter((page) =>
-            loggedIn ? !page.removeOnLogin : !page.requiresLogin
-          )
-          .map((page, count) => (
-            <div className={styles.navLink} key={count}>
-              <Link href={page.url}>
-                <a>{page.name}</a>
-              </Link>
-            </div>
-          ))}
+        {pages.map((page, count) => (
+          <div className={styles.navLink} key={count}>
+            <Link href={page.url}>
+              <a>{page.name}</a>
+            </Link>
+          </div>
+        ))}
+        {!session && (
+          <div className={styles.navLink + " " + styles.push}>
+            <a onClick={signIn}>Log in</a>
+          </div>
+        )}
       </nav>
-      <div
-        className={styles.toggle + " " + styles.push}
-        onClick={() => setMobileViewShown(!mobileViewShown)}
-      >
-        {mobileViewShown ? <FaTimes /> : <FaBars />}
-      </div>
+      <NavbarDropdown session={session} className={styles.userToggle} />
     </div>
   )
 }
