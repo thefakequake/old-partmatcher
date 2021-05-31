df9c0ff7d98ea525b53ad1f75776a0fe4805e565
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 22:38:31 +0100

    Add $slice to part comments (upcoming)

--- pages/api/parts/[id].js
+++ pages/api/parts/[id].js
@@ -4,7 +4,10 @@ export default async ({ query: { id } }, res) => {
   const { db } = await connectToDatabase()
   const part = await db
     .collection("Parts")
-    .findOne({ part_id: id }, { projection: { _id: 0 } })
+    .findOne(
+      { part_id: id },
+      { projection: { _id: 0, comments: { $slice: [0, 19] } } }
+    )
   if (part == null) {
     res.status(404).json({
       error: `Couldn't find a part with ID '${id}'`
