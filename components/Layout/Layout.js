3be6890033bffc8c8c9a6b76857c1eba6998b0d0
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 18:07:30 +0100

    Make navbar stay at the top of the screen

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <div id="pageContent">
+      <div id="pageContent" style={{marginTop: "5rem"}}>
         {children}
       </div>
       <Footer />
