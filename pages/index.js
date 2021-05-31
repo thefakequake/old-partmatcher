b83bcfe231b23c74d8f0672fb6c71e8699c84dc0
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:57:55 +0100

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Header from "../components/Header/Header"
+import React from "react"
+import { useSession } from "next-auth/client"
 
 export default function Home() {
+  const [session, loading] = useSession()
+
   return (
     <div>
       <Header />
+      {!session && (
+        <>
+          <h1>You aren't logged in.</h1>
+        </>
+      )}
+      {session && (
+        <>
+          <h1>{session.id}</h1>
+        </>
+      )}
       <h1>Welcome to PartMatcher!</h1>
     </div>
   )
