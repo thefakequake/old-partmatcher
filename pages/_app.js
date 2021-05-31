951f52e05c3fa30dbb4412ab40ecf68e4f3809da
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:57:01 +0100

    Add loading bar and auth wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
 import "../styles/globals.css"
 import Layout from "../components/Layout/Layout"
+import { Provider } from "next-auth/client"
+import NProgress from "nprogress"
+import Router from "next/router"
+import { useEffect } from "react"
+import { useSession } from "next-auth/client"
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 function MyApp({ Component, pageProps }) {
+  const [session, loading] = useSession()
+  const isUser = !!session?.user
+  useEffect(() => {
+    if (loading) return
+  }, [isUser, loading])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
