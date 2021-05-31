778ea06035b3fe29a69c92bd1edce0ce5f4efd20
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:59:10 +0100

    Create parts

---
+++ pages/parts/parts.js
@@ -0,0 +1,16 @@
+const parts = [
+  "CPU",
+  "Thermal Paste",
+  "CPU Cooler",
+  "Graphics Card",
+  "Motherboard",
+  "Memory",
+  "Solid State Drive",
+  "Hard Drive",
+  "WiFi Card",
+  "Case",
+  "Case fan",
+  "Power Supply"
+]
+
+export default parts
