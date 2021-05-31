2e362c86bb5fc1db2c1da7a461d7e82d0ef447dd
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:53:47 +0100

    Add className passthrough to logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -2,10 +2,10 @@ import styles from "./Logo.module.css"
 import Link from "next/link"
 import Image from "next/image"
 
-const Logo = () => {
+const Logo = ({ className }) => {
   return (
     <Link href="/">
-      <a className={styles.logo}>
+      <a className={styles.logo + ' ' + className}>
         <Image src="/images/logo.svg" width="58px" height="58px" alt="PartMatcher logo" />
         <h1>PartMatcher</h1>
       </a>
