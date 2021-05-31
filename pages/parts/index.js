b624a6dd80d484d40677ac6e9ced057ccceef37f
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:56:34 +0100

    Create parts page

---
+++ pages/parts/index.js
@@ -0,0 +1,25 @@
+import Header from "../../components/Header/Header"
+import PartChoice from "../../components/PartChoice/PartChoice"
+import styles from "./partsPage.module.css"
+import parts from "./parts"
+
+const partsPage = () => {
+  return (
+    <>
+      <Header
+        title="Parts"
+        description={`There are ${parts.length} part types available on PartMatcher.`}
+      />
+      <div className={styles.container}>
+        <h1 align="center" className={styles.titleText}>Part Types</h1>
+        <div className={styles.partTypes}>
+          {parts.map((part, count) => (
+            <PartChoice partType={part} key={count} className={styles.partType} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default partsPage
