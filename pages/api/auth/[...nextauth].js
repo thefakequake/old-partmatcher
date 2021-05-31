f7b9d53806154460a1bd097e3f0d5f8f55808055
QuaKe8782 <mastermind4560@gmail.com>
2021-05-31 10:58:04 +0100

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,59 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+import { accountAdapter } from "../../../utils/accountAdapter"
+
+const options = async () => ({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    Providers.Discord({
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET
+    })
+  ],
+  pages: {
+    signIn: "/login"
+  },
+
+  adapter: await accountAdapter(),
+
+  callbacks: {
+    async session(session, token) {
+      const newSession = {...session, id: token.id}
+      return newSession
+    },
+    signIn: async (profile, account, metadata) => {
+      if (account.provider != "github") 
+        return
+      const res = await fetch('https://api.github.com/user/emails', {
+        headers: {
+          'Authorization': `token ${account.accessToken}`
+        }
+      })
+      const emails = await res.json()
+      if (!emails || emails.length === 0) {
+        return
+      }
+      const sortedEmails = emails.sort((a, b) => b.primary - a.primary)
+      profile.email = sortedEmails[0].email
+    }
+  },
+
+  database: process.env.MONGO_URI,
+
+  session: {
+    jwt: false
+  }
+})
+
+export default async (req, res) => NextAuth(req, res, await options())
