7273399b4be81fc488ca7b230e34739b27437e16
QuaKe8782 <mastermind4560@gmail.com>
2021-06-01 18:53:19 +0100

    Fix issue with tooltips not displaying on CSR transition

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Router from "next/router"
 import { useState, useEffect } from "react"
 import { useSession } from "next-auth/client"
 import ReactTooltip from "react-tooltip"
+import { useRouter } from "next/router"
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -22,15 +23,25 @@ Router.onRouteChangeError = () => {
 function MyApp({ Component, pageProps }) {
   const [session, loading] = useSession()
   const [tooltipVisible, setTooltipVisibility] = useState(false)
+  const router = useRouter()
+  const [tooltipKey, setTooltipKey] = useState(0)
+
   const isUser = !!session?.user
   useEffect(() => {
-    setTooltipVisibility(true);
     if (loading) return
   }, [isUser, loading])
 
+  useEffect(() => {
+    setTooltipVisibility(true)
+  })
+
+  useEffect(() => {
+    setTooltipKey(prev => prev + 1)
+  }, [router.pathname])
+
   return (
     <Provider session={pageProps.session}>
-      {tooltipVisible && <ReactTooltip backgroundColor="#121212" />}
+      {tooltipVisible && <ReactTooltip backgroundColor="#121212" key={tooltipKey} />}
       <Layout>
         <Component {...pageProps} />
       </Layout>
