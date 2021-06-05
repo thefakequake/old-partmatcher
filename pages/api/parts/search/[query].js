27d9ce46c035dd7a79704ab345572d2e0271f0f3
QuaKe8782 <mastermind4560@gmail.com>
2021-06-05 16:49:09 +0100

    Improve part search API route

--- pages/api/parts/search/[query].js
+++ pages/api/parts/search/[query].js
@@ -1,7 +1,10 @@
 import { connectToDatabase } from "../../../../utils/mongodb"
 import clamp from "../../../../utils/clamp"
 
-export default async ({ query: { query = null, limit = 10, type = null } }, res) => {
+export default async (
+  { query: { query = null, limit = 10, type = null } },
+  res
+) => {
   if (query === null) {
     res.status(400).json({ error: "Please provide a query parameter" })
     return
@@ -19,12 +22,32 @@ export default async ({ query: { query = null, limit = 10, type = null } }, res)
   const { db } = await connectToDatabase()
   let parts = await db
     .collection("Parts")
-    .find({ $text: { $search: query } }, { projection: { _id: 0 }})
-    .limit(limit)
+    .aggregate([
+      {
+        $search: {
+          text: {
+            path: ["manufacturer", "name"],
+            query,
+            fuzzy: {
+              maxEdits: 2,
+              maxExpansions: 100
+            }
+          }
+        }
+      },
+      {
+        $limit: limit
+      },
+      {
+        $project: { _id: 0 }
+      }
+    ])
+    //.find({ $text: { $search: query } }, { projection: { _id: 0 }})
     .toArray()
- 
   if (type !== null) {
-    parts = parts.filter(part => part.type.toLowerCase() === type.toLowerCase())
+    parts = parts.filter(
+      (part) => part.type.toLowerCase() === type.toLowerCase()
+    )
   }
 
   res.status(200).json(parts)
