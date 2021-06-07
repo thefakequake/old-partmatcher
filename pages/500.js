a5d8b3d688f9ff0c677910cbcc13dafb16c823fa
QuaKe8782 <mastermind4560@gmail.com>
2021-06-07 20:19:57 +0100

    Replace error 505 with 500 as it should be

--- pages/500.js
+++ pages/500.js
@@ -1,13 +1,13 @@
-import styles from "../styles/505.module.css"
+import styles from "../styles/500.module.css"
 import Link from "next/link"
 import Header from "../components/Header/Header"
 
 const Custom404 = () => {
   return (
     <>
-      <Header title="Error 505" description="505: Internal server error" />
+      <Header title="Error 500" description="500: Internal server error" />
       <div className={styles.errorBox}>
-        <h1 className={styles.error}>505: Internal server error</h1>
+        <h1 className={styles.error}>500: Internal server error</h1>
         <h3 className={styles.errorText}>
           Something went wrong.{" "}
           <Link href="https://github.com/QuaKe8782/partmatcher/issues">
